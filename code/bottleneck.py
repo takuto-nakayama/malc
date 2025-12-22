4f0c16f6106d693ccd66c28d8130f6bd9b9e19b9
takuto-nakayama <tnakayama.a5ling@gmail.com>
2025-12-22 18:39:29 +0900

    20251222: fixed the twop distances

--- code/bottleneck.py
+++ code/bottleneck.py
@@ -11,26 +11,25 @@ import argparse, os
 if __name__ == '__main__':
 	## parsing arguments
 	parser = argparse.ArgumentParser()
-	parser.add_argument('path_h0', tyoe=str)
-	parser.add_argument('path_h1', type=str)
-	parser.add_argument('--save_path', type=str, default='../output/bottleneck.csv')
+	parser.add_argument('path', type=str)
+	parser.add_argument('id', type=str)
+	parser.add_argument('--save_path', type=str, default='../output')
 	args = parser.parse_args()
 
 	## defining variables
-	path_h0 = args.path_h0
-	path_h1 = args.path_h1
+	path = args.path
+	id = args.id
 	save_path = args.save_path
-	paths_h0 = os.listdir(path_h0)
-	paths_h1 = os.listdir(path_h1)
+	paths = os.listdir(path)
 	bot = []
 
 	## computing bottleneck distance
-	for h0_i, h1_i in zip(paths_h0, path_h1):
+	for i in paths:
 		bot_i = []
-		dgm_i = [np.load(h0_i), np.load(h1_i)]
-		for h0_j, h1_j in zip(path_h0, path_h1):
-			if h0_i != h0_j:
-				dgm_j = [np.load(h0_j), np.load(h1_j)]
+		dgm_i = np.load(f'{path}/{i}')
+		for j in paths:
+			if i != j:
+				dgm_j = np.load(f'{path}/{j}')
 				bot_i.append(bottleneck(dgm_i, dgm_j))
 			else:
 				bot_i.append(0)
@@ -38,9 +37,9 @@ if __name__ == '__main__':
 
 	## saving wasserstein distance matrix
 	df = pd.DataFrame(bot)
-	pd.to_csv(save_path)
+	df.to_csv(f'{save_path}/bottleneck-{id}.csv')
 
 	## visualizing result
 	sns.heatmap(df)
-	plt.savefig(f'{save_path[:-4]}.png')
+	plt.savefig(f'{save_path}/bottleneck-{id}.png')
 	plt.show()
--- code/pdgm.py
+++ code/pdgm.py
@@ -1,7 +1,7 @@
 #  importing modules
 from ripser import ripser
 import numpy as np
-import argparse, os
+import argparse, os, re
 
 
 #  main process
@@ -9,29 +9,22 @@ if __name__ == '__main__':
 	## parsing arguments
 	parser = argparse.ArgumentParser()
 	parser.add_argument('path', type=str)
+	parser.add_argument('id', type=str)
+	parser.add_argument('--sample_size', type=int, default=10000)
 	parser.add_argument('--metric', type=str, default='cosine')
 	parser.add_argument('--save_path', type=str, default='../output/pdgm/')
 	args = parser.parse_args()
 
 	## defining variables
 	path = args.path
+	id = args.id
+	sample_size = args.sample_size
 	metric = args.metric
 	save_path = args.save_path
 
 	## computing persistent homology
-	### if path is a file
-	embedding = np.load(path)
+	embedding = np.load(path)[:sample_size]
 	dgms = ripser(embedding, metric='cosine')['dgms']
 	### saving results
-	np.save(f'{save_path}/h0/{p}-h0.npy', dgms[0])
-	np.save(f'{save_path}/h1/{p}-h1.npy', dgms[1])
-
-	### if path is a directory
-	if os.path.isdir(path):
-		paths = os.listdir(path)
-	for p in paths:
-		embedding = np.load(p)
-		dgms = ripser(embedding, metric='cosine')['dgms']
-		### saving results
-		np.save(f'{save_path}/h0/{p}-h0.npy', dgms[0])
-		np.save(f'{save_path}/h1/{p}-h1.npy', dgms[1])
+	np.save(f'{save_path}/h0/{id}-h0.npy', dgms[0])
+	np.save(f'{save_path}/h1/{id}-h1.npy', dgms[1])
